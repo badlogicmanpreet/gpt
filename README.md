ec152584ed31e09664b577f663b26c133e27d46b
Manpreet Singh <manpreet.singh@192.168.1.242>
2024-06-07 12:18:58 +0100

    Transformer Decoder Implementation

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
-# Transformer 
-[<img src="images/transformer.jpeg" width="210"/>](images/transformer.jpeg)
+# Transformer: Decoder Implementation
+[<p align="center">
+ <img src="images/transformer_exp.png" width="600"/></p>](images/transformer_exp.png)
 
 ## Motivation
 
@@ -9,7 +10,7 @@ I initially began with a basic model using frequency as the foundation for gener
 
 ## Design [WIP]
 
-It's a simple design. I'll provide a brief overview. Keep [Transformer](images/transformer.jpeg) & [Code](src/transformer_decoder.py) open as a reference.
+It's a simple design. I'll provide a brief overview. Keep [Transformer](https://github.com/badlogicmanpreet/gpt/blob/main/images/transformer.jpeg) & [Code](https://github.com/badlogicmanpreet/gpt/blob/main/src/transformer_decoder.py) open as a reference.
 
 1. To begin, we define `tokens` and `position embeddings`. I utilize nn.Embedding for both, which is randomly initialized and will be learned over time. Token embeddings have a size equal to the number of tokens or vocabulary, with each embedding being of size n_embd. On the other hand, position embeddings have a size equal to the block size, where the block size is the length of each sequence. It's crucial to learn the spatial structure of each token in the sequence.
 
@@ -19,10 +20,15 @@ It's a simple design. I'll provide a brief overview. Keep [Transformer](images/t
 Communication refers to the layer in which tokens interact with each other. This layer is called the Multihead Attention layer and is composed of multiple heads.
     1. `Head` class is the single Head of Self Attention.
     2. Multiple heads combined for `MutiHeadAttention`.
+    [<p align="left">
+ <img src="images/multihead_form.png" width="280"/></p>](images/multihead_form.png)
     3. Let's understand the Head Class a bit more. This is where Self-Attention is developed. Go to the Section Transformer Head.
     4. Once you are done reading about the transformer head, it's time we look at the MultiHeadAttention. This is where we assemble multiple heads of self attention in parallel. 
         - Initialize nn.ModuleList to hold multiple Heads.
         - Under the forward method, pass the input through each head and finally concatenate on dimension -1.
+                    [<p align="left">
+ <img src="images/multihead.png" width="190"/></p>](images/multihead.png)
+
     5. If you run the model at this point, the generation result could be better, although it shows little improvement.
     6. Once the tokens have communicated using Head and MultiHeadAttention, it's time to move to computation. The FeedForward layer handles the computation.
         - It is a simple layer followed by a non-linearity.
@@ -34,6 +40,8 @@ Communication refers to the layer in which tokens interact with each other. This
     8. One important aspect within the Block is the Residual pathway. In the transformer image, look at the residual pathways. These are the arrows that go from input directly into the Add+Norm layer. Let's see why this is important. 
         - At a point where the neural networks are getting deeper and run into many optimization problems, we use an idea from the paper: You now add the skip or residual blocks. 
         - Refer to [Residual Blocks](https://towardsdatascience.com/residual-blocks-building-blocks-of-resnet-fd90ca15d6ec) article.
+            [<p align="left">
+ <img src="images/transformer-whole.png" width="250"/></p>](images/transformer-whole.png)
         - Linear projection on the residual pathways is essential so that the dimensions at the + are easy to add.
     9. Finally, a normalization layer and a linear layer are added.
 	  
@@ -57,8 +65,15 @@ We get the affinities between the tokens now by doing a dot product between the 
 - `transformer_decorder file implements the decoder block (nodes from the future never talk to the past nodes)`.
 - Unlike self-attention, where k, q, and v are working on the same node pool, cross-attention can have q from one node pool and k and v from another node pool. In this case, q is pulling information from the second node pool.
 - In the main attention formula, we also have to divide by sqrt(d_k). d_k is the head size, also called scaled attention. k and q are unit variance, i.e., unit Gaussian inputs. 'wei' will be unit variance, too. See below. 'Wei' is fed to Softmax. It's essential to keep the variance of wei near 1. If the variance is high, softmax will saturate, and if it is low, softmax will be very peaky.
+            [<p align="left">
+ <img src="images/attention.png" width="280"/></p>](images/attention.png)
 
-## Inspiration
+
+## Acknowledgements & Inspiration
+
+- Ashish Vaswani and the co-authors of the paper [Attention is All You Need.](https://proceedings.neurips.cc/paper_files/paper/2017/file/3f5ee243547dee91fbd053c1c4a845aa-Paper.pdf)
+- Ramiro Hum-Sah (https://stats.stackexchange.com/users/303928/ramiro-hum-sah), Why are residual connections needed in transformer architectures?, URL (version: 2022-02-21): https://stats.stackexchange.com/q/565196
+- Kaiming He and the co-authors of the paper [Deep Residual Learning for Image Recognition](https://arxiv.org/pdf/1512.03385)
 
 *Model implementations, so profoundly inspired by Andrej Karpathy's legendary work in neural networks*
 
--- notebooks/gpt.py
+++ notebooks/gpt.py
@@ -2,11 +2,11 @@ import torch
 import torch.nn as nn
 from torch.nn import functional as F
 
-# Transformer decoder - by Manpreet Singh
+# GPT - Generative Pre-trained Transformer - Rough Work by Manpreet Singh
 
 # hyperparameters
-batch_size = 128 # number of sequences in a batch
-block_size = 512 # length of each sequence
+batch_size = 64 # number of sequences in a batch
+block_size = 256 # length of each sequence
 max_iters = 5000
 eval_interval = 500
 learning_rate = 3e-4
@@ -20,7 +20,7 @@ dropout = 0.2
 torch.manual_seed(1337)
 
 # Read the input file and print the length of the text
-with open('dataset/input.txt', 'r', encoding='utf-8') as f:
+with open('/Users/manpreet.singh/git/gpt/notebooks/input.txt', 'r', encoding='utf-8') as f:
     text = f.read()
 
 # Create the vocabulary
@@ -39,15 +39,6 @@ train_data, valid_data = data[:n], data[n:]
 
 # data loader
 def get_batch(split):
-    """
-    Get a batch of data from the specified split.
-
-    Args:
-        split (str): The split to get the batch from. Can be 'train' or 'val'.
-
-    Returns:
-        tuple: A tuple containing the input sequences and target sequences.
-    """
     if split == 'train':
         data = train_data
     else:
@@ -60,12 +51,6 @@ def get_batch(split):
 
 @torch.no_grad()
 def estimate_loss():
-    """
-    Estimate the loss on the training and validation sets.
-
-    Returns:
-        dict: A dictionary containing the average loss on the training and validation sets.
-    """
     out = {}
     model.eval()
     for split in ['train', 'val']:
@@ -92,15 +77,6 @@ class Head(nn.Module):
         self.dropout = nn.Dropout(dropout)
 
     def forward(self, x):
-        """
-        Perform self-attention on the input sequence.
-
-        Args:
-            x (torch.Tensor): The input sequence of shape (batch_size, block_size, n_embd).
-
-        Returns:
-            torch.Tensor: The output sequence after self-attention of shape (batch_size, block_size, n_embd).
-        """
         B,T,C = x.shape
         k = self.key(x) # (B, T, C)
         q = self.query(x) # (B, T, C)
@@ -115,7 +91,7 @@ class Head(nn.Module):
         return out
 
 class MultiHeadAttention(nn.Module):
-    """Multiple heads of self attention in parallel"""
+    # multiple heads of self attention in parallel
     
     def __init__(self, num_heads, head_size):
         super().__init__()
@@ -124,21 +100,12 @@ class MultiHeadAttention(nn.Module):
         self.dropout = nn.Dropout(dropout)
     
     def forward(self, x):
-        """
-        Perform multi-head self-attention on the input sequence.
-
-        Args:
-            x (torch.Tensor): The input sequence of shape (batch_size, block_size, n_embd).
-
-        Returns:
-            torch.Tensor: The output sequence after multi-head self-attention of shape (batch_size, block_size, n_embd).
-        """
         out = torch.cat([h(x) for h in self.heads], dim=-1)
         out = self.dropout(self.proj(out))
         return out 
         
 class FeedFoward(nn.Module):
-    """A simple linear layer followed by a non-linearity"""
+    """ a simple linear layer followed by a non-linearity """
         
     def __init__(self, n_embd):
         super().__init__()
@@ -150,19 +117,16 @@ class FeedFoward(nn.Module):
         )
         
     def forward(self, x):
-        """
-        Perform feed-forward computation on the input sequence.
-
-        Args:
-            x (torch.Tensor): The input sequence of shape (batch_size, block_size, n_embd).
-
-        Returns:
-            torch.Tensor: The output sequence after feed-forward computation of shape (batch_size, block_size, n_embd).
-        """
         return self.net(x)
 
+# At this point the neural networks are getting deeper and run into many optimization problems. so we use idea from the paper where you now add the skip or residual blocks.
+# Refer residual blocks - https://towardsdatascience.com/residual-blocks-building-blocks-of-resnet-fd90ca15d6ec
+# Computation happens from top to bottom, and there exists a residual pathway, we can fork from the pathway and do some computation and then add it back to the main pathway. 
+#   So you go from input to output by using + and + and +. This is useful, from backpropagation history we know that for additive operations it distributes the gradients equally to
+#   each branch. So the gradients from the loss, hop using the additive operations back to the input and also fork off to the residual blocks. So the gradient flow easily to the input
+#   and the residual block has very small contributions at the beggining.
 class Block(nn.Module):
-    """Transformer block: communication followed by computation"""
+    """ Transformer block : communication followed by computation """
 
     def __init__(self, n_embd, n_head):
         super().__init__()
@@ -172,16 +136,8 @@ class Block(nn.Module):
         self.ln1 = nn.LayerNorm(n_embd)
         self.ln2 = nn.LayerNorm(n_embd)
         
+    # add x + for residual block
     def forward(self, x):
-        """
-        Perform communication and computation on the input sequence.
-
-        Args:
-            x (torch.Tensor): The input sequence of shape (batch_size, block_size, n_embd).
-
-        Returns:
-            torch.Tensor: The output sequence after communication and computation of shape (batch_size, block_size, n_embd).
-        """
         x = x + self.sa(self.ln1(x)) # apply one head of self-attention
         x = x + self.ffwd(self.ln2(x)) # apply feed forward layer
         return x
@@ -189,29 +145,34 @@ class Block(nn.Module):
 class GPT(nn.Module):
     def __init__(self):
         super().__init__()
+        # each token directly reads of the logits for the next token from the look up table
         self.token_embedding_table = nn.Embedding(vocab_size, n_embd) # embedding layer
         self.position_embedding_table = nn.Embedding(block_size, n_embd) # positional encoding, spatial information is important
+        #self.sa_heads = MultiHeadAttention(4, n_embd//4) # 4 heads of 8 dimensional self attention (here each head (communication channel) will give us 8 dimensions, so 4*8 = 32, which is the emb_size)
+        #self.ffwd = FeedForward(n_embd)
+        # self.blocks = nn.Sequential(
+        #     Block(n_embd, n_heads=4),
+        #     Block(n_embd, n_heads=4),
+        #     Block(n_embd, n_heads=4),
+        #     nn.LayerNorm(n_embd),
+        # )
         self.blocks = nn.Sequential(*[Block(n_embd, n_head=n_head) for _ in range(n_layer)]) # 4 transformer blocks
         self.ln_f = nn.LayerNorm(n_embd) # final layer norm
         self.lm_head = nn.Linear(n_embd, vocab_size) # linear layer
         
+        
     def forward(self, idx, targets=None):
-        """
-        Perform forward pass of the GPT model.
-
-        Args:
-            idx (torch.Tensor): The input sequence of shape (batch_size, block_size).
-            targets (torch.Tensor): The target sequence of shape (batch_size, block_size).
-
-        Returns:
-            tuple: A tuple containing the logits and loss (if targets are provided).
-        """
         B, T = idx.shape
 
+        # idx is the input to the model
         token_emb = self.token_embedding_table(idx) # (batch_size, block_size, emb_size) (B, T, C)
         pos_emb = self.position_embedding_table(torch.arange(T, device=device)) # (block_size, emb_size) (T, C)
-        x = token_emb + pos_emb # (batch_size, block_size, emb_size) (B, T, C)
 
+        x = token_emb + pos_emb # (batch_size, block_size, emb_size) (B, T, C)
+        # self attention is where tokens do communication and gather the data, once data is gathered they need to think on that individually. Feed forward layer is where they think on the data they have gathered
+        # works on individual tokens.
+        #x = self.sa_heads(x) # apply one head of self-attention (B, T, C)
+        #x = self.ffwd(x) # apply feed forward layer (B, T, C), after self attention we apply feed forward layer
         x = self.blocks(x) # apply the transformer blocks (B, T, C)
         x = self.ln_f(x) # apply final layer norm (B, T, C)
         logits = self.lm_head(x) # (batch_size, block_size, vocab_size) (B, T, vocab_size) e.g. (1, 32, 65), for 1 token batch size, 32 is the input and 65 is the output of linear layer
@@ -227,23 +188,21 @@ class GPT(nn.Module):
         return logits, loss
     
     def generate(self, idx, max_new_tokens):
-        """
-        Generate new tokens given an input sequence.
-
-        Args:
-            idx (torch.Tensor): The input sequence of shape (batch_size, block_size).
-            max_new_tokens (int): The maximum number of new tokens to generate.
-
-        Returns:
-            torch.Tensor: The generated sequence of shape (batch_size, block_size + max_new_tokens).
-        """
+        # idx is the input to the model (B*T)
         for _ in range(max_new_tokens):
-            idx_cond = idx[:, -block_size:] # crop idx to the last block_size tokens
+            # crop idx to the last block_size tokens
+            idx_cond = idx[:, -block_size:]
+            # get the predictions
             logits, loss = self(idx_cond) # (B*T, C)
-            logits = logits[:, -1, :] # focus on the last time step only
-            probs = F.softmax(logits, dim=-1) # apply softmax to get the probabilities
-            next_token = torch.multinomial(probs, num_samples=1) # sample from the distribution
-            idx = torch.cat([idx, next_token], dim=1) # append sampled index to the running sequence
+            # focus on the last time step only
+            logits = logits[:, -1, :] # get the logits for the last token (B, C), we use only the last token to predict the next token
+            # apply softmax to get the probabilities
+            probs = F.softmax(logits, dim=-1) # (B, C)
+            # sample from the distribution
+            next_token = torch.multinomial(probs, num_samples=1) # (B, 1)
+            # append sampled index to the running sequence
+            idx = torch.cat([idx, next_token], dim=1) # (B, T+1)
+            #print(idx)
         return idx
 
 model = GPT()
@@ -256,9 +215,11 @@ optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
 for iter in range(max_iters):
 
     if iter % eval_interval == 0:
+        # estimate the loss on the validation set
         losses = estimate_loss()
         print(f"step: {iter}, train loss: {losses['train']:.4f}, valid loss: {losses['val']:.4f}")
 
+    # get the batch
     xb, yb = get_batch('train')
 
     # evaluate the loss
